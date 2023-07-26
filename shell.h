ce384bea124a32ccedf2cf49a1e6201951c558c3
teargas001 <nkimnjoro@gmail.com>
2023-07-26 00:49:21 -0700

    Header file

--- shell.h
+++ shell.h
@@ -1,41 +1,75 @@
 #ifndef SHELL_H
 #define SHELL_H
 
+/* Libraries */
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <string.h>
+#include <errno.h>
 
 /* Constants */
-#define MAX_INPUT_LENGTH 1024
-#define MAX_TOKENS 64
-#define TOKEN_DELIMITERS " \t\r\n\a"
-#define PROMPT "simple_shell$ "
+#define BUF_SIZE 1024
+#define DELIM " \t\r\n\a"
+#define MAX_ARGS 64
 
-/* Function Declarations */
+/* Global Variables */
+extern char **environ;
+
+/* Function Prototypes */
+
+/* Command.c */
 void display_prompt(void);
-char *read_line(void);
-char **split_line(char *line);
-void free_args(char **args);
-int execute_command(char **args); /* Modify the function signature */
-void handle_exit(char **args);
-void handle_env(char **args);
-void handle_cd(char **args);
-void handle_setenv(char **args);
-void handle_unsetenv(char **args);
-void handle_alias(char **args);
-void handle_separator(char **args);
-void handle_logic(char **args);
-void handle_variables(char **args);
-void handle_comments(char **args);
-void handle_input_file(const char *filename);
-void handle_input(int argc, char **argv);
-char **get_path(void);
-int execute_pipeline(char **pipeline, int pipeline_length);
-int execute_command_with_arguments(char **args);
-char *build_path(char *path, char *command);
-int check_command(char *command, char **paths);
+char *read_command(void);
+
+/* lines.c */
+char **parse_arguments(char *line);
+
+/* path.c */
+char *find_path(char *command);
+
+/* exit.c */
+int sh_exit(void);
+
+/* built.c */
+int sh_env(void);
+
+/* getline.c */
+char *sh_getline(void);
+
+/* strtok.c */
+char *sh_strtok(char *str, const char *delim);
+
+/* builtin.c */
+int sh_exit_status(char **args);
+
+/* setenv.c */
+int sh_setenv(char **args);
+int sh_unsetenv(char **args);
+
+/* cd.c */
+int sh_cd(char **args);
+
+/* ;separator.c */
+int sh_execute_with_separators(char *line);
+
+/* logic.c */
+int sh_execute_with_logic_operators(char *line);
+
+/* alias.c */
+int sh_alias(char **args);
+
+/* Variables.c */
+char *replace_variables(char *line);
+char *replace_dollar_question(char *line);
+char *replace_dollar_dollar(char *line);
+
+/* Comments.c */
+void remove_comments(char *line);
+
+/* input.c */
+int run_commands_from_file(char *filename);
 
 #endif /* SHELL_H */
