bb973a269aec50d6d85ac18f8bfb5765f5af3d22
teargas001 <nkimnjoro@gmail.com>
2023-07-26 01:53:12 -0700

    Command

--- command.c
+++ command.c
@@ -1,66 +1,124 @@
 #include "shell.h"
 
 /**
- * display_prompt - Displays the shell prompt.
+ * prompt - displays the prompt for the shell
  */
-void display_prompt(void)
+
+void prompt(void)
 {
-	printf("$ ");
-}
+	int _prompt;
 
-/**
- * read_command - Reads a command line from the user.
- *
- * Return: A pointer to the command line input or NULL on error.
- */
-char *read_command(void)
-{
-	char *line = NULL;
-	size_t bufsize = 0;
-	ssize_t characters_read;
-
-	characters_read = getline(&line, &bufsize, stdin);
-
-	if (characters_read == -1)
+	_prompt = write(STDOUT_FILENO, "$ ", 2); /* Display prompt */
+	if (_prompt == -1)
 	{
-		if (feof(stdin))
-		{
-			/* Handle EOF (Ctrl+D) */
-			putchar('\n');
-			exit(sh_exit());
-		}
-		else
-		{
-			/* Handle read error */
-			perror("read_command");
-			free(line);
-			exit(EXIT_FAILURE);
-		}
+		perror("Error: ");
+		exit(EXIT_FAILURE);
 	}
-
-	return (line);
 }
 
-/**
- * main - The main function that runs the shell.
- *
- * Return: Always 0.
- */
-int main(void)
-{
-	char *command_line;
+int updated = 0;
 
+/**
+ * main - the entry point of the simple shell
+ * @ac: command line argument count
+ * @av: string containing the command line argument
+ * Return: 0 on success
+ */
+
+int main(int ac __attribute__((unused)), char **av __attribute__((unused)))
+{
+	size_t bufsize = 0;
+	ssize_t characters;
+	char *status = NULL, *exit_str = "exit ", *status_copy, *dup;
+
+	cmd = NULL;
+	signal(SIGINT, handle_signal);
 	while (1)
 	{
-		display_prompt();
-		command_line = read_command();
-
-		/* Execute the command */
-		printf("Executing: %s", command_line);
-
-		free(command_line);
+		if (isatty(STDIN_FILENO))
+			prompt();
+		characters = getline(&cmd, &bufsize, stdin);
+		if (characters == -1)
+		{
+			break;
+		}
+		cmd[characters - 1] = '\0';
+		if (_strlen(cmd) == 0 || _strspn(cmd, " \t\r\n") ==
+			(size_t)_strlen(cmd))
+			continue;
+		/* check for comments, handle_comment modifies cmd */
+ 		if (handle_comment(cmd) == NULL)
+ 		{
+			free(cmd);
+			cmd = NULL;
+			continue;
+		}
+		if (_strcmp(cmd, "env") == 0)
+		{
+			handle_env();
+			free(cmd);
+			cmd = NULL;
+			continue;
+		}
+		if (_strcmp(cmd, "exit") == 0)
+		{
+			free(cmd);
+			cmd = NULL;
+			exit_builtin(cmd, NULL);
+			continue;
+		}
+		/* Handle cd command */
+		dup = _strdup(cmd);
+		if (_strcmp(strtok(dup, " \t\r\n"), "cd") == 0)
+		{
+			change_dir(cmd);
+			free(cmd);
+			free(dup);
+			dup = cmd = NULL;
+			continue;
+		}
+		free(dup);
+		if (_strncmp(cmd, exit_str, _strlen(exit_str)) == 0)
+		{
+			status = cmd + _strlen(exit_str);
+			if (status[0] == '\0')
+				status = NULL;
+			status_copy = (status != NULL) ? _strdup(status) : NULL;
+			free(cmd);
+			cmd = NULL;
+			exit_builtin(status_copy, status_copy);
+			free(status_copy);
+			status_copy = NULL;
+			continue;
+		}
+		execute_cmd(cmd, NULL);
+		free(cmd);
+		cmd = NULL;
 	}
-
+	free(cmd);
+	cmd = NULL;
 	return (0);
 }
 
+/**
+ * handle_signal - signal handler when ctrl + c is pressed
+ * @sig: holds the SIGINT
+ * Return: nothing
+ */
+void handle_signal(int sig)
+{
+	write(1, "\n", 1);
+	if (sig == SIGINT)
+	{
+		if (cmd != NULL)
+		{
+			free(cmd);
+			cmd = NULL;
+		}
+		if (updated == 1)
+		{
+			free_environ(environ);
+		}
+		exit(0);
+	}
+}
